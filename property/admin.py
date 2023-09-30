b6e7b7d58329857ad4acddb0e5ab92e0ebcd4444
anton <anton@adminpc.Dlink>
2023-09-30 14:52:56 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,8 @@ from .models import Flat, Claim
 class FaltAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'owners_phonenumber', 'owner_pure_phone', 'new_building', 'construction_year',
+                    'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('like',)
